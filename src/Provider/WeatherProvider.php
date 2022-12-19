5cf4c726d18f5f6c492d2eab7478b22edc30c360
feng.pan <feng.pan@mail.com>
2022-12-19 21:54:57 +0800

    添加LaravelProvider

---
+++ src/Provider/WeatherProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace FlowerAllure\ComposerUtils\App\Provider;
+
+use FlowerAllure\ComposerUtils\App\Services\WeatherServer;;
+
+class WeatherProvider extends \Illuminate\Support\ServiceProvider
+{
+    protected $defer = true;
+
+    public function register()
+    {
+        $this->app->singleton(WeatherServer::class, function(){
+            return new WeatherServer(config('services.weather.key'));
+        });
+
+        // $this->app->alias(WeatherServer::class, 'weather');
+    }
+
+    public function provides()
+    {
+        return [WeatherServer::class];
+    }
+}
