a08c9ed7f16e34631f5f0517854dd8a7cfcf79fc
feng.pan <feng.pan@mail.com>
2022-12-19 21:59:10 +0800

    修复php-cs-fixer.sh

--- php-cs-fixer.sh
+++ php-cs-fixer.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-# PHP_CS_FIXER_IGNORE_ENV=1 ./vendor/bin/php-cs-fixer fix --config=.php-cs-fixer.php ./
-PHP_CS_FIXER_IGNORE_ENV=1 ./vendor/bin/php-cs-fixer fix ./
+PHP_CS_FIXER_IGNORE_ENV=1 ./vendor/bin/php-cs-fixer fix --config=.php-cs-fixer.php ./
