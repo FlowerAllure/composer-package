1855563ed5e28e21aed0aa618e7d36c37e08b945
feng.pan <feng.pan@mail.com>
2022-12-27 22:06:39 +0800

    update build.sh

--- shell/build.sh
+++ shell/build.sh
@@ -19,17 +19,16 @@ Usage: bash --branch=master --services=service1,service2
 EOF
 }
 
-branch() {
+branch_build() {
     exec_command 'git fetch --tags --progress'
-    exec_command 'git rev-parse -q --verify origin/master^{commit}'
-    local commit=exec_command 'git rev-parse -q --verify origin/master^{commit}'
+    local commit=$(exec_command "git rev-parse origin/${1}^{commit}")
     exec_command "git checkout -f ${commit}"
 }
 
 parse_business() {
     case "$1" in
-      "--branch=")
-        branch ${2}
+      "--branch")
+        branch_build ${2}
         ;;
       *)
         usage
@@ -56,80 +55,3 @@ for param in ${params[@]}
 do
     parse_params $param
 done
-
-
-exit;
-echo
-
-
-service supervisord restart
-service supervisord start
-
-exit;
-
-
-
-
-rm -rf $DIR/bootstrap/cache/config.php $DIR/bootstrap/cache/services.php $DIR/bootstrap/cache/packages.php
-cd $DIR
-# shellcheck disable=SC1083
-git fetch && COMMIT=`git rev-parse refs/remotes/origin/master^{commit}` && git checkout -f $COMMIT
-/usr/local/php/bin/php $DIR/artisan config:cache
-chmod 777 -R $DIR/bootstrap/ $DIR/storage/
-ls $DIR/ | grep -v 'public' | xargs chown nginx:nginx -R
-chown nginx:nginx $DIR/public
-# shellcheck disable=SC2010
-cd $DIR/public/ && ls $DIR/public/ | grep -v 'files' | xargs chown nginx:nginx -R
-
-
-
-#!/bin/bash
-cd /var/www/cris_project
-git pull origin master
-DIR=/var/www/cris30_project
-rm -rf $DIR/bootstrap/cache/config.php $DIR/bootstrap/cache/services.php $DIR/bootstrap/cache/packages.php
-cd $DIR
-git pull origin master
-/usr/local/php/bin/php $DIR/artisan config:cache
-chmod 777 -R $DIR/bootstrap/ $DIR/storage/
-# shellcheck disable=SC2010
-ls $DIR/ | grep -v 'public' | xargs chown nginx:nginx -R
-chown nginx:nginx $DIR/public
-# shellcheck disable=SC2010
-cd $DIR/public/ && ls $DIR/public/ | grep -v 'files' | xargs chown nginx:nginx -R
-
-
-#!/bin/bash
-cd /var/www/cris_project
-git pull origin master
-service supervisord stop
-DIR=/var/www/cris30_project
-rm -rf $DIR/bootstrap/cache/config.php $DIR/bootstrap/cache/services.php $DIR/bootstrap/cache/packages.php
-cd $DIR
-git pull origin master
-/usr/local/php/bin/php $DIR/artisan config:cache
-chmod 777 -R $DIR/bootstrap/ $DIR/storage/
-# shellcheck disable=SC2010
-ls $DIR/ | grep -v 'public' | xargs chown nginx:nginx -R
-chown nginx:nginx $DIR/public
-# shellcheck disable=SC2010
-cd $DIR/public/ && ls $DIR/public/ | grep -v 'files' | xargs chown nginx:nginx -R
-service supervisord start
-
-
-#!/bin/bash
-cd /var/www/cris_portal
-git pull origin master
-DIR=/var/www/cris30_portal
-cd $DIR
-git pull origin master
-/usr/local/php/bin/php $DIR/artisan config:cache
-/usr/local/php/bin/php $DIR/artisan migrate
-chmod 777 -R $DIR/bootstrap/ $DIR/storage/
-# shellcheck disable=SC2011
-ls $DIR/ | xargs chown nginx:nginx -R
-/usr/local/php/bin/php $DIR/artisan project:db
-/usr/local/php/bin/php $DIR/artisan project:table
-/usr/local/php/bin/php $DIR/artisan project:auth
-
-
