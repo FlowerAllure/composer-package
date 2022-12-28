67b2f9028d1fc0e6ae33690315a040348c81b83d
feng.pan <feng.pan@mail.com>
2022-12-28 16:16:08 +0800

    🎨 Apply PHP CS Fixer

--- ci/batch-cs-fix.sh
+++ ci/batch-cs-fix.sh
@@ -6,8 +6,7 @@ cd "${START_PATH}/.."
 PHP_CS_FIXER_IGNORE_ENV=1 ./vendor/bin/php-cs-fixer fix --config=.php-cs-fixer.php ./
 git diff --exit-code --color=always
 status=$?
-if [[ $? -ne 0 ]]; then
+if [[ $status -ne 0 ]]; then
     echo -e "\033[1;33m🎨 请更正代码中的格式错误\033[0m"
-    echo $status
     exit $status
 fi
