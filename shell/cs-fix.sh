c62b8c7d78046f47c169bf89034e5a4619d22f71
feng.pan <feng.pan@mail.com>
2022-12-28 12:48:42 +0800

    udpate cs-fix.sh

--- shell/cs-fix.sh
+++ shell/cs-fix.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 
-exit_command() {
-    echo -e "\033[1;33m参数错误\033[0m"
-    exit $1
-}
-
-usage() {
-    cat << EOF
-Usage: bash ./shell/build.sh --branch=master --services=service1,service2
-EOF
-}
-
 check_style() {
     PHP_CS_FIXER_IGNORE_ENV=1 ./vendor/bin/php-cs-fixer fix --config=.php-cs-fixer.php $1 && git diff --exit-code --color=always $1
     if [[ $? -ne 0 ]]; then
